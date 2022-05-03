6583ab63376731693e0eb2315a38476054bfcf67
Mykyta <mykyta.y@hotmail.com>
2022-05-03 15:15:14 -0300

    set up routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CommHistory from './components/commHistory/commHistory';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/history" element={<CommHistory/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/commHistory/commHistory.js
+++ src/components/commHistory/commHistory.js
@@ -1,5 +1,5 @@
 import React from "react";
-import image from '../image.png';
+import image from '../../image.png';
 import './commHistory.css';
 
 const CommHistory = () => {
