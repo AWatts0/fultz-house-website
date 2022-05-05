0a6139daa0218302db902bd5eb6fc3491e48353b
Ewan MacLean <git config --global user.email ewanmaclean77@gmail.comgit config --global user.email>
2022-05-05 14:20:29 -0300

    added navbar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from './components/homepage';
 import CommHistory from './components/commHistory/commHistory';
+import MyNavbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <MyNavbar/>
         <Routes>
           <Route path="/" element={<Homepage/>}></Route>
           <Route path="/history" element={<CommHistory/>}></Route>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,32 +8,91 @@ import {
     Button,
   } from "react-bootstrap";
   import { Link } from "react-router-dom";
-//   import "./navbar.css";
+  import "./navbar.css";
 
-<Navbar bg="light" expand="lg">
-  <Container fluid>
-    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-    <Navbar.Toggle aria-controls="navbarScroll" />
-    <Navbar.Collapse id="navbarScroll">
-      <Nav
-        className="me-auto my-2 my-lg-0"
-        style={{ maxHeight: '100px' }}
-        navbarScroll
-      >
-        <Nav.Link href="#action1">Home</Nav.Link>
-        <Nav.Link href="#action2">Link</Nav.Link>
-        <NavDropdown title="Link" id="navbarScrollingDropdown">
-          <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action5">
-            Something else here
-          </NavDropdown.Item>
-        </NavDropdown>
-        <Nav.Link href="#" disabled>
-          Link
-        </Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+function MyNavbar() {
+    return(
+
+        <Navbar bg="" className="navbar" expand="lg" variant="dark">
+            <Container fluid>
+                <Navbar.Brand href="#">Fultz House</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                <Nav
+                    className="me-auto my-2 my-lg-0"
+                    style={{ maxHeight: '100px' }}
+                    navbarScroll
+                    >
+                    <NavDropdown title="About" id="navbarScrollingDropdown">
+                        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action5">
+                            Something else here
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Archives & Collections" id="navbarScrollingDropdown">
+                        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action5">
+                            Something else here
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Media" id="navbarScrollingDropdown">
+                        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action5">
+                            Something else here
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Events" id="navbarScrollingDropdown">
+                        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action5">
+                            Something else here
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Programming" id="navbarScrollingDropdown">
+                        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action5">
+                            Something else here
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Contribute" id="navbarScrollingDropdown">
+                        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action5">
+                            Something else here
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Shop" id="navbarScrollingDropdown">
+                        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action5">
+                            Something else here
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Contact" id="navbarScrollingDropdown">
+                        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action5">
+                            Something else here
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                    
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+            </Navbar>
+          )
+      }
+
+export default MyNavbar
