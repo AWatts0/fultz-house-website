f9c15bdd54d5fff24e8acfb140a11c04e3cbc131
Adam Watts <a_watts@outlook.com>
2022-05-16 09:09:55 -0300

    made placeholder components

--- src/App.js
+++ src/App.js
@@ -11,6 +11,15 @@ import ContactUs from './components/contactUs/contact';
 import Photos from './components/gallery/photos/Photos.js';
 import EventsPage from './components/events/events';
 import Volunteer from './components/volunteer/volunteer';
+import Careers from './components/careers/careers'
+import Facility from './components/facility/facility';
+import Donations from './components/donations/donations';
+import Governace from './components/governace/governace';
+import Kids from './components/kids/kids';
+import Presentations from './components/presentations/presentations';
+import Programming from './components/programming/programming';
+import Rentals from './components/rentals/rentals';
+import Videos from './components/videos/videos';
 
 function App() {
   return (
@@ -25,7 +34,16 @@ function App() {
           <Route path='/notablepeople' element={<NotablePeople />}></Route>
           <Route path='/photo' element={<Photos />}></Route>
           <Route path='/events' element={<EventsPage />}></Route>
+          <Route path='/careers' element={<Careers />}></Route>
           <Route path='/volunteer' element={<Volunteer />}></Route>
+          <Route path='/donations' element={<Donations />}></Route>
+          <Route path='/facilty' element={<Facility />}></Route>
+          <Route path='/governance' element={<Governace />}></Route>
+          <Route path='/kids' element={<Kids />}></Route>
+          <Route path='/presentations' element={<Presentations />}></Route>
+          <Route path='/programming' element={<Programming />}></Route>
+          <Route path='/rentals' element={<Rentals />}></Route>
+          <Route path='/videos' element={<Videos />}></Route>
         </Routes>
       </BrowserRouter>
         <div id="footer">
