d385e2af875409e8d84bd704d932ff7b4602f0b3
Ewan MacLean <git config --global user.email ewanmaclean77@gmail.comgit config --global user.email>
2022-05-19 14:49:55 -0300

    side button styling

--- src/components/sideButtons/sideButtons.js
+++ src/components/sideButtons/sideButtons.js
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 const SideButtons = function() {
     return (
         <div className="side-buttons">
-                    <a href='/donate'  className='link'>
+                    <a href='/donate'  className='side-link'>
                         <div className='side-button'>
                                 <p>Donate</p>
                         </div>
                     </a>
-                    <a href='/volunteer' className='link'>
+                    <a href='/volunteer' className='side-link'>
                         <div className='side-button'>
                                 <p>Volunteer</p>
                         </div>
